899818241fe2fe6db5a16152d67fd79a5962053b
Indigo <bill@wadsweb.com>
2025-08-10 13:32:08 +0100

    added auth from blog post gsgx dot com

---
+++ auth.c
@@ -0,0 +1,152 @@
+#include <security/pam_appl.h>
+#include <security/pam_misc.h>
+
+#include <pwd.h>
+#include <paths.h>
+
+#include "auth.h"
+
+#define SERVICE_NAME "display_manager"
+
+static void init_env(struct passwd *pw);
+static void set_env(char *name, char *value);
+static int end(int last_result);
+
+static int conv(int num_mesg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr);
+
+static pam_handle_t *pam_handle;
+
+/* Helper function for login to determine and act upon any PAM errors that may happen */
+bool pam_err(int result, char* name) {
+    if (result != PAM_SECCESS) {
+        fprintf(stderr, "%s: %s\n", name, pam_strerror(pam_handle, result));
+        end(result);
+        return true;
+    }
+    return false;
+}
+
+bool login(const char *username, const char *password, pid_t *child_pid) {
+    const char *data[2] = {username, password};
+    struct pam_conv pam_conv = {
+        conv, data
+    };
+
+    int result = pam_start(SERVICE_NAME, username, &pam_conv, &pam_handle);
+    if (pam_err(result, "pam_start")) return false;
+
+    result = pam_authenticate(pam_handle, 0);
+    if (pam_err(result, "pam_authenticate")) return false;
+
+    result = pam_acct_mgmt(pam_handle, 0);
+    if (pam_err(result, "pam_acct_mgmt")) return false;
+
+    result = pam_setcred(pam_handle, PAM_ESTABLISH_CRED);
+    if (pam_err(result, "pam_setcred")) return false;
+
+    result = pam_open_session(pam_handle, 0);
+    if (pam_err(result, "pam_open_session")) {
+        pam_setcred(pam_handle, PAM_DELETE_CRED);
+        return false;
+    }
+
+    struct passwd *pw = getpwnam(username);
+    init_env(pw);
+
+    *child_pid = fork();
+    if (*child_pid == 0) {
+        chdir(pw->pw_dir);
+        char *cmd = "exec /bin/bash --login .xinitrc";
+        execl(pw->pw_shell, pw->pw_shell, "-c", cmd, NULL);
+        printf("Failed to boot Window Manager");
+        exit(1);
+    }
+    return true;
+}
+
+static int conv(int num_mesg, const struct pam_message **msg, struct pam_response **resp, void *appdata_ptr) {
+    *resp = calloc(num_mesg, sizeof(struct pam_response));
+    if (*resp == NULL) {
+        return PAM_BUF_ERR;
+    }
+
+    int result = PAM_SUCCESS;
+    for (int i=0; i < num_mesg; i++) {
+        char *username;
+        char *password;
+
+        switch(msg[i]->msg_style) {
+            case PAM_PROMPT_ECHO_ON:
+                username = ((char **) appdata_ptr)[0];
+                (*resp)[i].resp = strdup(username);
+                break;
+            case PAM_PROMPT_ECHO_OFF:
+                password = ((char **) appdata_ptr)[1];
+                (*resp)[i].resp = strdup(password);
+                break;
+            case PAM_ERROR_MSG:
+                fprintf(stderr, "%s\n", msg[i]->msg);
+                result=PAM_CONV_ERR;
+                break;
+            case PAM_TEXT_INFO:
+                printf("%s\n", msg[i]->msg);
+                break;
+        }
+        if (result != PAM_SUCCESS) {
+            break;
+        }
+    }
+    if (result != PAM_SUCCESS) {
+        free(*resp);
+        *resp=0;
+    }
+
+    return result;
+}
+
+static void init_env(struct passwd *pw) {
+    set_env("HOME", pw->pw_dir);
+    set_env("PWD", pw->pw_dir);
+    set_env("SHELL", pw->pw_shell);
+    set_env("USER", pw->pw_name);
+    set_env("LOGNAME", pw->pw_name);
+    set_env("PATH", "/usr/local/sbin:/usr/local/bin:/usr/bin");
+    set_env("MAIL", _PATH_MAILDIR);
+
+    size_t xauthority_len = strlen(pw->pw_dir) + strlen("/.Xauthority") + 1;
+    char *xauthority = malloc(xauthority_len);
+    snprintf(xauthority, xauthority_len, "%s/.Xauhtority", pw->pw_dir);
+    set_env("XAUTHORITY", xauthority);
+    free(xauthority);
+}
+
+static void set_env(char *name, char *value) {
+    // plus 2 to account for the equal + null terminal bytes
+    size_t name_value_len = strlen(name) + strlen(value) + 2;
+    char *name_value = malloc(name_value_len);
+    snprintf(name_value, name_value_len, "%s=%s", name, value);
+    pam_putenv(pam_handle, name_value);
+    free(name_value);
+}
+
+bool logout() {
+    int result = pam_close_session(pam_handle);
+    if (pam_err(result, "pam_close_session")) {
+        pam_setcred(pam_handle, PAM_DELETE_CRED);
+        return false;
+    }
+
+    result = pam_setcred(pam_handle, PAM_DELETE_CRED);
+    if (pam_err(result, "pam_setcred")) {
+        return false;
+    }
+
+    end(result);
+    return true;
+}
+
+static int end(int last_result) {
+    int result = pam_end(pam_handle, last_result);
+    pam_handle = 0;
+    return result;
+}
--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 #include <sys/ioctl.h>
 #include <unistd.h>
 
+#include "auth.h"
+
 int ROWS;
 int COLS;
 
@@ -22,5 +24,21 @@ int main() {
     for (int i=0; i<COLS; i++) {
         printf("i");
     }
+
+    char username[32];
+    const char *usernamePoint = &username[0];
+    char passwd[32];
+    const char *passwdPoint = &passwd[0];
+
+    // INSECURE AND ONLY HERE FOR TESTING PURPOSES
+    // DO NOT USE THIS WITH REAL DETAILS
+    printf("Wagwan, whats your username: ");
+    scanf("%s", &username);
+    printf("\nWhats your password Gng: ");
+    scanf("%s", &passwd);
+
+    printf("%s, %s", username, passwd);
+    
+    login(usernamePoint, passwdPoint, 0);
     return 0;
 }
