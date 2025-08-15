57a00c7343071d2998571045fc798af6edf58158
Indigo <bill@wadsweb.com>
2025-08-15 23:16:06 +0100

    extracted input feilds and a basic function

--- main.c
+++ main.c
@@ -9,6 +9,7 @@
 // FOR THE BOX
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <ncurses.h>
 #include <panel.h>
 
@@ -16,6 +17,7 @@
 
 #define BOX_HEIGHT 10
 #define BOX_LENGTH 50
+#define INPUT_MAX_SIZE 32 
 
 const char *logo = 
 "${c1}         -/oyddmdhs+:. \n"
@@ -40,8 +42,17 @@ const char *logo =
 int ROWS;
 int COLS;
 
+struct inputBox {
+    WINDOW *win;
+    char input[INPUT_MAX_SIZE];
+    int inputSize;
+    bool showChar; 
+};
+
 void drawAsciiArt(WINDOW *win, int y, int x, char *string);
 void mvwaddncolstr(WINDOW *win, int y, int x, char *string, int n);
+struct inputBox newInputWindow(WINDOW *win, int height, int length, int y, int x, char *name, bool showChar);
+void writeCharToInputWindow(struct inputBox *box, char c);
 
 void mvwaddncolstr(WINDOW *win, int y, int x, char *string, int n) {
     wmove(win, y, x);
@@ -71,30 +82,63 @@ void wdrawAsciiArt(WINDOW *win, int y, int x, char *string) {
     }
 }
 
+struct inputBox newInputWindow(WINDOW *win, int height, int length, int y, int x, char *name, bool showChar) {
+    WINDOW *input_box = subwin(win, height, length, y, x);
+    box(input_box, 0, 0);
+    mvwprintw(input_box, 0, 1, name);
+    touchwin(win);
+    wrefresh(input_box);
+    char inputFeild[INPUT_MAX_SIZE];
+    struct inputBox ret = {
+        input_box,
+        *inputFeild,
+        0,
+        showChar,
+    };
+    return ret;
+}
+
+void writeCharToInputWindow(struct inputBox *box, char c) {
+    if (box->inputSize >= INPUT_MAX_SIZE-1) {
+        return;
+    }
+    box->input[box->inputSize] = c;
+    // always insert a null pointer afterwards
+    // its just safer
+    box->input[box->inputSize+1] = '\0';
+    // if we should hide the entered character
+    if (box->showChar == false) {
+        //TODO: why is this always being run? 
+        //c = '*';
+    }
+    // add 4 to account for some padding on the left hand size
+    mvwaddch(box->win, 1, box->inputSize+4, c);
+    box->inputSize++;
+    wrefresh(box->win);
+}
+
 int main() {
 
     // Get Window Size
     struct winsize w;
     ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
-    ROWS = w.ws_row - 5; // -1 is temporary to ensure that the console line doe not push first line off screen
+    ROWS = w.ws_row;
     COLS = w.ws_col;
 
-    //TODO: MAKE UI WITH CURSES
-    //IMPORT ALL THE STUFF FROM THE TEST DIR
-    //AND ALSO DECIDE ON A UI LAYOUT
-    // 
-    // NEW UI STARTS HERE
     initscr();
     keypad(stdscr, TRUE);
     noecho();
     
     // NEED SOME COLOr
+    // This is only for the ascii art and this will need to change in the future for it to work with
+    // the art stolen from neofetch, so ... thatll be fun to make
     start_color();
     init_pair(1, COLOR_MAGENTA, COLOR_BLACK);
     init_pair(2, COLOR_WHITE, COLOR_BLACK);
     refresh();
 
-    //MOVE TO SOMEWHERE NICER
+    //move to be export variables
+    //However I fear what variable size ascci art will do, i did not check if they are consistent sizes
     int window_height = 22;
     int window_length = 100;
     int gap = 6;
@@ -119,33 +163,28 @@ int main() {
     touchwin(win);
     wrefresh(launch_box);
 
-    WINDOW *login_box = subwin(win, 3, input_length, begin_y+12, begin_x+window_length/2);
-    box(login_box, 0, 0);
-    mvwprintw(login_box, 0, 1, " USERNAME ");
-    touchwin(win);
-    wrefresh(login_box);
 
-    WINDOW *password_box = subwin(win, 3, input_length, begin_y+17, begin_x+window_length/2);
-    box(password_box, 0, 0);
-    mvwprintw(password_box, 0, 1, " PASSWORD ");
-    touchwin(win);
-    wrefresh(password_box);
+    struct inputBox login_box = newInputWindow(win, 3, input_length, begin_y+12, begin_x+window_length/2, " USERNAME ", true);
 
+    struct inputBox password_box = newInputWindow(win, 3, input_length, begin_y+17, begin_x+window_length/2, " PASSWORD ", false);
+
+    //TODO: Need to ensure that ascii art is either consistently sized in neofetch repo or add some code to center the ascii art here
     wdrawAsciiArt(win, 2, 6, logo);
     wrefresh(win);
 
     //POPULATE SOME EXAMPLE DATA
     mvwprintw(launch_box, 1, 3, "X SERVER");
     wrefresh(launch_box);
-    mvwprintw(login_box, 1, 3, "soup");
-    wrefresh(login_box);
-    mvwprintw(password_box, 1, 3, "********");
-    wrefresh(password_box);
+    mvwprintw(password_box.win, 1, 4, "********");
+    wrefresh(password_box.win);
 
 
     char b;
     do {
-        b = getch(); //just wait for now before shutting down UI
+        //just wait for now before shutting down UI
+        b = getch(); 
+        // test to see if it works
+        writeCharToInputWindow(&login_box, b);
     } while(b != 'q');
     endwin();
     exit(0);
